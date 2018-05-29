0f5fb6e939d9fecf2c9fa542543964b42dbae4e3
1210964167 <1210964167@qq.com>
2018-05-30 00:08:23 +0800

    host

--- bottle/bottle/settings.py
+++ bottle/bottle/settings.py
@@ -73,12 +73,22 @@ WSGI_APPLICATION = 'bottle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'test',
+#         'USER': 'root',
+#         'PASSWORD': 'skk619',
+#         'HOST': 'localhost',
+#         'PORT': 3306,
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bottle',
         'USER': 'root',
-        'PASSWORD': 'skk619',
+        'PASSWORD': 'wlbottle2018!',
         'HOST': 'localhost',
         'PORT': 3306,
     }
