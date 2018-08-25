287869bbb5f405780f53151f1f1b53529c8b3e60
1210964167 <1210964167@qq.com>
2018-08-25 14:03:03 +0800

    mobile good

--- bottle/goods/urls.py
+++ bottle/goods/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^ueditor/', include('DjangoUeditor.urls')),
-    url(r'type/(?P<type_good>.*)/', views.index)
+    url(r'type/(?P<type_good>.*)/', views.index),
+    url(r'mobile/type/(?P<type_good>.*)/', views.mobile_index)
 ]
--- bottle/goods/views.py
+++ bottle/goods/views.py
@@ -4,7 +4,14 @@ from goods.models import Products
 
 # Create your views here.
 def index(request, type_good):
-    print(type_good)
+    # print(type_good)
     products = Products.objects.get(category=type_good)
     all_products = Products.objects.all()
     return render(request, "glassware.html", {"products": products, "all_products": all_products})
+
+
+def mobile_index(request, type_good):
+    # print(type_good)
+    products = Products.objects.get(category=type_good)
+    all_products = Products.objects.all()
+    return render(request, "glasswareMobile.html", {"products": products, "all_products": all_products})
